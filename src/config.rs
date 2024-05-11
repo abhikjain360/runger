e576895d4a28fb2b8a4cf75a967ebc8ace07e0d4
Abhik Jain <abhik@abhikjain.xyz>
2024-05-11 14:06:20 +0530

    minor refactor

--- src/config.rs
+++ src/config.rs
@@ -2,11 +2,12 @@ use std::{num::NonZeroUsize, path::Path};
 
 use mlua::{Lua, Table};
 
-use crate::Result;
+use crate::{cli::LogLevel, Result};
 
 #[derive(Debug, Clone)]
 pub struct Config {
     pub required_columns: NonZeroUsize,
+    pub log_level: LogLevel,
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -20,27 +21,31 @@ impl Default for Config {
         Self {
             // SAFETY: it is not zero
             required_columns: unsafe { NonZeroUsize::new_unchecked(3) },
+            log_level: LogLevel::Error,
         }
     }
 }
 
 impl Config {
     pub fn new(path: impl AsRef<Path>) -> Result<Self> {
-        let default = Self::default();
+        let mut config = Self::default();
 
         if !path.as_ref().exists() {
-            return Ok(default);
+            return Ok(config);
         }
 
         let lua = Lua::new();
-        let config: Table = lua.load(path.as_ref()).eval()?;
+        let table: Table = lua.load(path.as_ref()).eval()?;
 
-        let required_columns = match config.get::<_, Option<usize>>("required_columns")? {
+        match table.get::<_, Option<usize>>("required_columns")? {
             Some(0) => return Err(Error::ZeroRequiredColumns.into()),
-            None => default.required_columns,
-            Some(val) => NonZeroUsize::new(val).ok_or_else(|| Error::ZeroRequiredColumns)?,
+            Some(val) => {
+                config.required_columns =
+                    NonZeroUsize::new(val).ok_or_else(|| Error::ZeroRequiredColumns)?;
+            }
+            _ => {}
         };
 
-        Ok(Self { required_columns })
+        Ok(config)
     }
 }
